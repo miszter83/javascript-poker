4be2e4b169180060bb57b5ea98a37b67fa7828cd
miszter83 <miszter83@gmail.com>
2023-07-11 17:37:52 +0200

    játékos lapjainak a húzása

--- poker.js
+++ poker.js
@@ -1,1 +1,39 @@
-alert('hello, JavaSript!');
+const newGameButton = document.querySelector('.js-new-game-button');
+const playerCardsContainer = document.querySelector('.js-player-cards-container');
+
+
+
+
+// program state
+let deckId = null;
+let playerCards = [];
+
+function renderPlayerCards() {
+    let html = '';
+
+    for (let card of playerCards) {
+        html += `<img src="${ card.image }" alt="${ card.code }"></img>`;
+    }            
+    playerCardsContainer.innerHTML = html;
+
+}
+
+function drawAndRenderPlayerCards() {
+    if (deckId == null) return;
+    fetch(`https://www.deckofcardsapi.com/api/deck/${ deckId }/draw/?count=2`)
+        .then(data => data.json())
+        .then(response => {
+            playerCards = response.cards;
+            renderPlayerCards();
+        });
+}
+function startGame() {
+    fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(data => data.json())
+        .then(response => {
+            deckId = response.deck_id;
+            drawAndRenderPlayerCards(); //todo: refactorálás async-await segítségével
+});
+}
+
+newGameButton.addEventListener('click', startGame);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Poker JavaScript játék API segítsévégel
+
+## Felhasznált eszközök
+- https://www.deckofcardsapi.com/
+- `button-15` a https://getcssscan.com/css-buttons-examples oldalról
